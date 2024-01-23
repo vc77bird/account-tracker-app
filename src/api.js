cc8596f666d2140d32a9bd949073564d5847deb2
Bird <vc77bird@gmail.com>
2024-01-23 10:48:21 -0500

    Add .env and connection to the DB depended on env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
+let baseURL = 'http://oncore-ontrack.umms.umm.edu:8000'
+
+try {
+    if (process.env.REACT_APP_baseURL)
+        baseURL = process.env.REACT_APP_baseURL
+} catch (error) {
+    console.log('Error', error);
+}
+console.log(baseURL);
+
+
 const api = axios.create({
-    baseURL: 'http://oncore-ontrack.umms.umm.edu:8000',
+    baseURL: baseURL,
 });
 
 export default api;
